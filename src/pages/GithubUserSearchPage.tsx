3e2e4fe9413f9e7b1b1811f28f995ac554e67cd2
hakmoune <mehdi.hakmoune@gmail.com>
2024-07-15 22:17:27 +0200

    Create the mock with JSX and CSS

---
+++ src/pages/GithubUserSearchPage.tsx
@@ -0,0 +1,240 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan, faCopy } from "@fortawesome/free-solid-svg-icons";
+import "../css/GithubUserSearchPage.css";
+
+export default function GithubUserSearchPage() {
+  return (
+    <div className="container">
+      <div className="title">
+        <h4>Github Search</h4>
+      </div>
+      <div className="input">
+        <input type="text" placeholder="Search input" className="input-field" />
+      </div>
+      <div className="actions">
+        <span className="selected-items">
+          <input type="checkbox" /> 3 elements selected
+        </span>
+        <span>
+          <a href="#">
+            <FontAwesomeIcon icon={faTrashCan} className="custom-icon" />
+          </a>
+          <a href="#">
+            <FontAwesomeIcon icon={faCopy} className="custom-icon" />
+          </a>
+        </span>
+      </div>
+      <div className="users-container">
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+
+        <div className="item">
+          <input type="checkbox" className="checkbox" />
+          <ul className="info">
+            <li>image</li>
+            <li>id</li>
+            <li>login</li>
+          </ul>
+          <button className="btn-profile">View profile</button>
+        </div>
+      </div>
+    </div>
+  );
+}
